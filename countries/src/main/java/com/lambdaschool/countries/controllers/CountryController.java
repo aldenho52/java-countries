f932a378c761ca16925a93db0306685ee74c97e7
aldenho52 <69052933+aldenho52@users.noreply.github.com>
2020-12-03 14:24:12 -0700

    names/all get rquest working

--- countries/src/main/java/com/lambdaschool/countries/controllers/CountryController.java
+++ countries/src/main/java/com/lambdaschool/countries/controllers/CountryController.java
@@ -1,9 +1,16 @@
 package com.lambdaschool.countries.controllers;
 
 
+import com.lambdaschool.countries.models.Country;
 import com.lambdaschool.countries.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CountryController
@@ -11,5 +18,18 @@ public class CountryController
     @Autowired
     CountryRepository countryrepos;
 
+    //    http://localhost:8080/names/all
+    @GetMapping(value="/names/all", produces = "application/json")
+    public ResponseEntity<?> listAllCountries()
+    {
+        List<Country> myList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(myList::add);
+        myList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+        return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
 
+    //    http://localhost:8080/names/start/u
+    //    http://localhost:8080/population/total
+    //    http://localhost:8080/population/min
+    //    http://localhost:8080/population/max
 }
